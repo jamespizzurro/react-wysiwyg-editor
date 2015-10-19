98aa17ab540f5b2e948f823e9258eaa74b9f1779
Phil Myers <phil.myers@it.com>
2015-10-19 16:55:53 -0400

    Remove dependencies on bootstrap.js, use react-bootstrap instead

--- EditableDiv.js
+++ EditableDiv.js
@@ -1,6 +1,15 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var React = require('react');
+var ReactBootstrap = require('react-bootstrap');
+var ButtonGroup = ReactBootstrap.ButtonGroup;
+var DropdownButton = ReactBootstrap.DropdownButton;
+var MenuItem = ReactBootstrap.MenuItem;
+var Button = ReactBootstrap.Button;
+var OverlayTrigger = ReactBootstrap.OverlayTrigger;
+var Popover = ReactBootstrap.Popover;
 
 module.exports = React.createClass({
 	displayName: 'EditableDiv',
@@ -10,182 +19,228 @@ module.exports = React.createClass({
 		onChange: React.PropTypes.func.isRequired
 	},
 
-	getInitialState: function() {
+	getInitialState: function getInitialState() {
 		// this is anti-pattern but we treat this.props.content as initial content
-		return {html: this.props.content};
+		return {
+			html: this.props.content,
+			paragraphStyle: 'none',
+			fontSizeStyle: 'none',
+			alignStyle: 'none'
+		};
 	},
 
-	emitChange: function() {
+	emitChange: function emitChange() {
 		var editor = this.refs.editor.getDOMNode(),
-			newHtml = editor.innerHTML;
+		    newHtml = editor.innerHTML;
 
-		this.setState({html: newHtml}, function() {
+		this.setState({ html: newHtml }, (function () {
 			this.props.onChange({
 				target: {
 					value: newHtml
 				}
 			});
-		}.bind(this));
+		}).bind(this));
 	},
 
-	componentWillReceiveProps: function(nextProps) {
+	componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
 		this.setState({
 			html: nextProps.content
 		});
 	},
 
-	shouldComponentUpdate: function(nextProps) {
-        return nextProps.content !== this.state.html;
-    },
+	shouldComponentUpdate: function shouldComponentUpdate(nextProps) {
+		return nextProps.content !== this.state.html;
+	},
 
-    execCommand: function(command, arg) {
-    	document.execCommand(command, false, arg);
-    },
+	execCommand: function execCommand(command, arg) {
+		document.execCommand(command, false, arg);
+	},
 
-	render: function() {
+	_createLink: function _createLink() {
+		this.execCommand('createLink', this.refs.linkUrl.getDOMNode().value);
+	},
+
+	toggleDropdown: function toggleDropdown(dropdown) {
+		var dropdownObject = {};
+		dropdownObject[dropdown] = this.state[dropdown] === 'none' ? 'block' : 'none';
+		this.setState(dropdownObject);
+	},
+
+	render: function render() {
 		// customize css rules here
-		var buttonSpacing = {marginRight: 2},
-			toolbarStyle = {marginBottom: 3};
-
-		return (
-			React.createElement("div", null, 
-				React.createElement("div", {style: toolbarStyle}, 
-
-					/** feel free to customize buttons below. 
-					for list of supported commands, please see https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand */
-					React.createElement("div", {className: "btn-group", style: buttonSpacing}, 
-						React.createElement("button", {
-							className: "btn btn-default btn-xs dropdown-toggle", 
-							type: "button", "data-toggle": "dropdown", 
-							"aria-expanded": "true"}, 
-							React.createElement("i", {className: "fa fa-paragraph"}), " ", React.createElement("i", {className: "fa fa-caret-down"})
-						), 
-						React.createElement("ul", {className: "dropdown-menu", role: "menu"}, 
-							React.createElement("li", null, 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'formatBlock', 'P')}, 
-									"Paragraph"
-								), 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'formatBlock', 'BLOCKQUOTE')}, 
-									"Block Quote"
-								), 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'formatBlock', 'H1')}, 
-									"Header 1"
-								), 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'formatBlock', 'H2')}, 
-									"Header 2"
-								), 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'formatBlock', 'H3')}, 
-									"Header 3"
-								), 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'formatBlock', 'H4')}, 
-									"Header 4"
-								), 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'formatBlock', 'H5')}, 
-									"Header 5"
-								), 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'formatBlock', 'H6')}, 
-									"Header 6"
-								)
-							)
-						)
-					), 
-
-					React.createElement("div", {className: "btn-group btn-group-xs", role: "group", style: buttonSpacing}, 
-						React.createElement("button", {type: "button", className: "btn btn-default", onClick: this.execCommand.bind(this, 'bold')}, 
-							React.createElement("i", {className: "fa fa-bold"})
-						), 
-						React.createElement("button", {type: "button", className: "btn btn-default", onClick: this.execCommand.bind(this, 'italic')}, 
-							React.createElement("i", {className: "fa fa-italic"})
-						), 
-						React.createElement("button", {type: "button", className: "btn btn-default", onClick: this.execCommand.bind(this, 'underline')}, 
-							React.createElement("i", {className: "fa fa-underline"})
-						), 
-						React.createElement("button", {type: "button", className: "btn btn-default", onClick: this.execCommand.bind(this, 'strikeThrough')}, 
-							React.createElement("i", {className: "fa fa-strikethrough"})
-						), 
-
-						React.createElement("div", {className: "btn-group", role: "group"}, 
-							React.createElement("button", {
-								className: "btn btn-default btn-xs dropdown-toggle", 
-								type: "button", "data-toggle": "dropdown", 
-								"aria-expanded": "true"}, 
-								React.createElement("i", {className: "fa fa-text-height"}), " ", React.createElement("i", {className: "fa fa-caret-down"})
-							), 
-							React.createElement("ul", {className: "dropdown-menu", role: "menu"}, 
-								React.createElement("li", null, 
-									React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'fontSize', 1)}, "1")
-								), 
-								React.createElement("li", null, 
-									React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'fontSize', 2)}, "2")
-								), 
-								React.createElement("li", null, 
-									React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'fontSize', 3)}, "3")
-								), 
-								React.createElement("li", null, 
-									React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'fontSize', 4)}, "4")
-								), 
-								React.createElement("li", null, 
-									React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'fontSize', 5)}, "5")
-								), 
-								React.createElement("li", null, 
-									React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'fontSize', 6)}, "6")
-								), 
-								React.createElement("li", null, 
-									React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'fontSize', 7)}, "7")
-								)
-							)
-						)
-					), 
-
-					React.createElement("div", {className: "btn-group btn-group-xs", role: "group", style: buttonSpacing}, 
-						React.createElement("button", {type: "button", className: "btn btn-default", onClick: this.execCommand.bind(this, 'insertOrderedList')}, 
-							React.createElement("i", {className: "fa fa-list-ol"})
-						), 
-						React.createElement("button", {type: "button", className: "btn btn-default", onClick: this.execCommand.bind(this, 'insertUnorderedList')}, 
-							React.createElement("i", {className: "fa fa-list-ul"})
-						)
-					), 
-
-					React.createElement("div", {className: "btn-group", style: buttonSpacing}, 
-						React.createElement("button", {
-							className: "btn btn-default btn-xs dropdown-toggle", 
-							type: "button", 
-							"data-toggle": "dropdown", 
-							"aria-expanded": "false"}, 
-							React.createElement("i", {className: "fa fa-align-left"}), " ", React.createElement("i", {className: "fa fa-caret-down"})							
-						), 
-						React.createElement("ul", {className: "dropdown-menu", role: "menu"}, 
-							React.createElement("li", null, 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'justifyLeft')}, "Align Left")
-							), 
-							React.createElement("li", null, 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'justifyRight')}, "Align Right")
-							), 
-							React.createElement("li", null, 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'justifyCenter')}, "Align Center")
-							), 
-							React.createElement("li", null, 
-								React.createElement("a", {href: "javascript:;", onClick: this.execCommand.bind(this, 'justifyFull')}, "Align Justify")
-							)
-						)
-					), 
-
-					React.createElement("button", {
-						type: "button", 
-						className: "btn btn-default btn-xs", 
-						onClick: this.execCommand.bind(this, 'removeFormat')}, 
-						React.createElement("i", {className: "fa fa-eraser"})
-					)
-				), 
-
-				React.createElement("div", React.__spread({
-					ref: "editor", 
-					className: "form-control"}, 
-					this.props, 
-					{contentEditable: "true", 
-					dangerouslySetInnerHTML: {__html: this.state.html}, 
-					onInput: this.emitChange}))
+		var buttonSpacing = { marginRight: 2 },
+		    toolbarStyle = { marginBottom: 3 };
+		var createLinkForm = React.createElement(
+			'div',
+			null,
+			React.createElement('input', { type: 'text', ref: 'linkUrl', autofocus: true }),
+			React.createElement(
+				Button,
+				{ onClick: this._createLink },
+				'Done'
 			)
+		);
+
+		return React.createElement(
+			'div',
+			null,
+			React.createElement(
+				'div',
+				{ style: toolbarStyle },
+				React.createElement(
+					ButtonGroup,
+					null,
+					React.createElement(
+						DropdownButton,
+						{ title: React.createElement('i', { className: 'fa fa-paragraph' }), id: 'bg-nested-dropdown' },
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'formatBlock', 'P') },
+							'Paragraph'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'formatBlock', 'BLOCKQUOTE') },
+							'Block Quote'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'formatBlock', 'H1') },
+							'Header 1'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'formatBlock', 'H2') },
+							'Header 2'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'formatBlock', 'H3') },
+							'Header 3'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'formatBlock', 'H4') },
+							'Header 4'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'formatBlock', 'H5') },
+							'Header 5'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'formatBlock', 'H6') },
+							'Header 6'
+						)
+					),
+					React.createElement(
+						Button,
+						{ onClick: this.execCommand.bind(this, 'bold') },
+						React.createElement('i', { className: 'fa fa-bold' })
+					),
+					React.createElement(
+						Button,
+						{ onClick: this.execCommand.bind(this, 'italic') },
+						React.createElement('i', { className: 'fa fa-italic' })
+					),
+					React.createElement(
+						Button,
+						{ onClick: this.execCommand.bind(this, 'underline') },
+						React.createElement('i', { className: 'fa fa-underline' })
+					),
+					React.createElement(
+						Button,
+						{ onClick: this.execCommand.bind(this, 'strikeThrough') },
+						React.createElement('i', { className: 'fa fa-strikethrough' })
+					),
+					React.createElement(
+						DropdownButton,
+						{ title: React.createElement('i', { className: 'fa fa-text-height' }), id: 'bg-nested-dropdown' },
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'fontSize', 1) },
+							'1'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'fontSize', 2) },
+							'2'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'fontSize', 3) },
+							'3'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'fontSize', 4) },
+							'4'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'fontSize', 5) },
+							'5'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'fontSize', 6) },
+							'6'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'fontSize', 7) },
+							'7'
+						)
+					),
+					React.createElement(
+						Button,
+						{ onClick: this.execCommand.bind(this, 'insertOrderedList') },
+						React.createElement('i', { className: 'fa fa-list-ol' })
+					),
+					React.createElement(
+						Button,
+						{ onClick: this.execCommand.bind(this, 'insertUnorderedList') },
+						React.createElement('i', { className: 'fa fa-list-ul' })
+					),
+					React.createElement(
+						DropdownButton,
+						{ title: React.createElement('i', { className: 'fa fa-align-left' }), id: 'bg-nested-dropdown' },
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'justifyLeft') },
+							'Align Left'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'justifyRight') },
+							'Align Right'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'justifyCenter') },
+							'Align Center'
+						),
+						React.createElement(
+							MenuItem,
+							{ onSelect: this.execCommand.bind(this, 'justifyFull') },
+							'Align Justify'
+						)
+					),
+					React.createElement(
+						Button,
+						{ onClick: this.execCommand.bind(this, 'removeFormat') },
+						React.createElement('i', { className: 'fa fa-eraser' })
+					)
+				)
+			),
+			React.createElement('div', _extends({
+				ref: 'editor',
+				className: 'form-control'
+			}, this.props, {
+				contentEditable: 'true',
+				dangerouslySetInnerHTML: { __html: this.state.html },
+				onInput: this.emitChange }))
 		);
 	}
 });
--- EditableDiv.jsx
+++ EditableDiv.jsx
@@ -1,5 +1,12 @@
 'use strict';
 var React = require('react');
+var ReactBootstrap = require('react-bootstrap');
+var ButtonGroup = ReactBootstrap.ButtonGroup;
+var DropdownButton = ReactBootstrap.DropdownButton;
+var MenuItem = ReactBootstrap.MenuItem;
+var Button = ReactBootstrap.Button;
+var OverlayTrigger = ReactBootstrap.OverlayTrigger;
+var Popover = ReactBootstrap.Popover;
 
 module.exports = React.createClass({
 	displayName: 'EditableDiv',
@@ -11,12 +18,28 @@ module.exports = React.createClass({
 
 	getInitialState: function() {
 		// this is anti-pattern but we treat this.props.content as initial content
-		return {html: this.props.content};
+		return {
+			html: this.props.content
+		};
 	},
 
-	emitChange: function() {
-		var editor = this.refs.editor.getDOMNode(),
-			newHtml = editor.innerHTML;
+	componentWillReceiveProps: function(nextProps) {
+		this.setState({
+			html: nextProps.content
+		});
+	},
+
+	shouldComponentUpdate: function(nextProps) {
+		return nextProps.content !== this.state.html;
+	},
+
+	_execCommand: function(command, arg) {
+		document.execCommand(command, false, arg);
+	},
+
+	_emitChange: function() {
+		var editor = this.refs.editor.getDOMNode();
+		var newHtml = editor.innerHTML;
 
 		this.setState({html: newHtml}, function() {
 			this.props.onChange({
@@ -27,163 +50,69 @@ module.exports = React.createClass({
 		}.bind(this));
 	},
 
-	componentWillReceiveProps: function(nextProps) {
-		this.setState({
-			html: nextProps.content
-		});
-	},
-
-	shouldComponentUpdate: function(nextProps) {
-        return nextProps.content !== this.state.html;
-    },
-
-    execCommand: function(command, arg) {
-    	document.execCommand(command, false, arg);
-    },
-
 	render: function() {
 		// customize css rules here
-		var buttonSpacing = {marginRight: 2},
-			toolbarStyle = {marginBottom: 3};
+		var toolbarStyle = {marginBottom: 3};
 
 		return (
 			<div>
 				<div style={toolbarStyle}>
-
-					{/** feel free to customize buttons below. 
-					for list of supported commands, please see https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand */}
-					<div className="btn-group" style={buttonSpacing}>
-						<button 
-							className="btn btn-default btn-xs dropdown-toggle" 
-							type="button" data-toggle="dropdown" 
-							aria-expanded="true">
-							<i className="fa fa-paragraph"></i> <i className="fa fa-caret-down"></i>
-						</button>
-						<ul className="dropdown-menu" role="menu">
-							<li>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'formatBlock', 'P')}>
-									Paragraph
-								</a>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'formatBlock', 'BLOCKQUOTE')}>
-									Block Quote
-								</a>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'formatBlock', 'H1')}>
-									Header 1
-								</a>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'formatBlock', 'H2')}>
-									Header 2
-								</a>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'formatBlock', 'H3')}>
-									Header 3
-								</a>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'formatBlock', 'H4')}>
-									Header 4
-								</a>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'formatBlock', 'H5')}>
-									Header 5
-								</a>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'formatBlock', 'H6')}>
-									Header 6
-								</a>
-							</li>
-						</ul>
-					</div>
-
-					<div className="btn-group btn-group-xs" role="group" style={buttonSpacing}>
-						<button type="button" className="btn btn-default" onClick={this.execCommand.bind(this, 'bold')}>
+					<ButtonGroup>
+						<DropdownButton title={<i className="fa fa-paragraph"></i>} id="bg-nested-dropdown">
+							<MenuItem onSelect={this._execCommand.bind(this, 'formatBlock', 'P')}>Paragraph</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'formatBlock', 'BLOCKQUOTE')}>Block Quote</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'formatBlock', 'H1')}>Header 1</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'formatBlock', 'H2')}>Header 2</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'formatBlock', 'H3')}>Header 3</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'formatBlock', 'H4')}>Header 4</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'formatBlock', 'H5')}>Header 5</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'formatBlock', 'H6')}>Header 6</MenuItem>
+						</DropdownButton>
+						<Button onClick={this._execCommand.bind(this, 'bold')}>
 							<i className="fa fa-bold"></i>
-						</button>
-						<button type="button" className="btn btn-default" onClick={this.execCommand.bind(this, 'italic')}>
+						</Button>
+						<Button onClick={this._execCommand.bind(this, 'italic')}>
 							<i className="fa fa-italic"></i>
-						</button>
-						<button type="button" className="btn btn-default" onClick={this.execCommand.bind(this, 'underline')}>
+						</Button>
+						<Button onClick={this._execCommand.bind(this, 'underline')}>
 							<i className="fa fa-underline"></i>
-						</button>
-						<button type="button" className="btn btn-default" onClick={this.execCommand.bind(this, 'strikeThrough')}>
+						</Button>
+						<Button onClick={this._execCommand.bind(this, 'strikeThrough')}>
 							<i className="fa fa-strikethrough"></i>
-						</button>
-
-						<div className="btn-group" role="group">
-							<button 
-								className="btn btn-default btn-xs dropdown-toggle" 
-								type="button" data-toggle="dropdown" 
-								aria-expanded="true">
-								<i className="fa fa-text-height"></i> <i className="fa fa-caret-down"></i>
-							</button>
-							<ul className="dropdown-menu" role="menu">
-								<li>
-									<a href="javascript:;" onClick={this.execCommand.bind(this, 'fontSize', 1)}>1</a>
-								</li>
-								<li>
-									<a href="javascript:;" onClick={this.execCommand.bind(this, 'fontSize', 2)}>2</a>
-								</li>
-								<li>
-									<a href="javascript:;" onClick={this.execCommand.bind(this, 'fontSize', 3)}>3</a>
-								</li>
-								<li>
-									<a href="javascript:;" onClick={this.execCommand.bind(this, 'fontSize', 4)}>4</a>
-								</li>
-								<li>
-									<a href="javascript:;" onClick={this.execCommand.bind(this, 'fontSize', 5)}>5</a>
-								</li>
-								<li>
-									<a href="javascript:;" onClick={this.execCommand.bind(this, 'fontSize', 6)}>6</a>
-								</li>
-								<li>
-									<a href="javascript:;" onClick={this.execCommand.bind(this, 'fontSize', 7)}>7</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-
-					<div className="btn-group btn-group-xs" role="group" style={buttonSpacing}>
-						<button type="button" className="btn btn-default" onClick={this.execCommand.bind(this, 'insertOrderedList')}>
-							<i className="fa fa-list-ol"></i>
-						</button>
-						<button type="button" className="btn btn-default" onClick={this.execCommand.bind(this, 'insertUnorderedList')}>
-							<i className="fa fa-list-ul"></i>
-						</button>
-					</div>
-
-					<div className="btn-group" style={buttonSpacing}>
-						<button
-							className="btn btn-default btn-xs dropdown-toggle"
-							type="button"
-							data-toggle="dropdown"
-							aria-expanded="false">
-							<i className="fa fa-align-left"></i> <i className="fa fa-caret-down"></i>							
-						</button>
-						<ul className="dropdown-menu" role="menu">
-							<li>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'justifyLeft')}>Align Left</a>
-							</li>
-							<li>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'justifyRight')}>Align Right</a>
-							</li>
-							<li>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'justifyCenter')}>Align Center</a>
-							</li>
-							<li>
-								<a href="javascript:;" onClick={this.execCommand.bind(this, 'justifyFull')}>Align Justify</a>
-							</li>
-						</ul>
-					</div>
-
-					<button 
-						type="button" 
-						className="btn btn-default btn-xs" 
-						onClick={this.execCommand.bind(this, 'removeFormat')}>
-						<i className="fa fa-eraser"></i>
-					</button>
+						</Button>
+						<DropdownButton title={<i className="fa fa-text-height"></i>} id="bg-nested-dropdown">
+							<MenuItem onSelect={this._execCommand.bind(this, 'fontSize', 1)}>1</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'fontSize', 2)}>2</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'fontSize', 3)}>3</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'fontSize', 4)}>4</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'fontSize', 5)}>5</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'fontSize', 6)}>6</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'fontSize', 7)}>7</MenuItem>
+						</DropdownButton>
+						<Button onClick={this._execCommand.bind(this, 'insertOrderedList')}>
+							<i className="fa fa-list-ol"></i>	
+						</Button>
+						<Button onClick={this._execCommand.bind(this, 'insertUnorderedList')}>
+							<i className="fa fa-list-ul"></i>	
+						</Button>
+						<DropdownButton title={<i className="fa fa-align-left"></i>} id="bg-nested-dropdown">
+							<MenuItem onSelect={this._execCommand.bind(this, 'justifyLeft')}>Align Left</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'justifyRight')}>Align Right</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'justifyCenter')}>Align Center</MenuItem>
+							<MenuItem onSelect={this._execCommand.bind(this, 'justifyFull')}>Align Justify</MenuItem>
+						</DropdownButton>
+						<Button onClick={this._execCommand.bind(this, 'removeFormat')}>
+							<i className="fa fa-eraser"></i>
+						</Button>
+					</ButtonGroup>
 				</div>
-
 				<div
 					ref="editor"
 					className="form-control"
 					{...this.props} 
 					contentEditable="true"
 					dangerouslySetInnerHTML={{__html: this.state.html}}
-					onInput={this.emitChange} />
+					onInput={this._emitChange}/>
 			</div>
 		);
 	}
